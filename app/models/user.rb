b4a3b69a5ddd69b26d0ddad656019d0bbb12ebf7
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-08-11 05:24:11 +0000

    [Add]4-14:usersコントローラー作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ApplicationRecord
 
   has_many :post_images, dependent: :destroy #Userモデルに対してPostImageモデルが1:Nになるよう関連付け
 
+  has_one_attached :profile_image #profile_imageという名前でActiveStorageでプロフ画像を保存できるように設定
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed #このメソッドに対して引数を設定したため、受け取った引数のサイズに変換できる
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-
+  get "homes/about" => "homes#about", as: "about"
   resources :post_images, only: [:new, :create, :index, :show, :destroy] #自動作成された3つを1行に変更後、create.destroy追加
-
-  get 'homes/about' => "homes#about", as: 'about'
+  resources :users, only: [:show, :edit] #自動作成された2つを1行にした
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
