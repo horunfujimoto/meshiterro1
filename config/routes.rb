81fc289b949b97bacd0ad607eed17f045e181f58
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-08-11 07:36:46 +0000

    [Add]4-16:マイページ編集機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "homes/about" => "homes#about", as: "about"
   resources :post_images, only: [:new, :create, :index, :show, :destroy] #自動作成された3つを1行に変更後、create.destroy追加
-  resources :users, only: [:show, :edit] #自動作成された2つを1行にした
+  resources :users, only: [:show, :edit, :update] #自動作成された2つを1行にした
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
