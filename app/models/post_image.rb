543d44bda0ab7b75eefb528b6f09b0ee9a53352a
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-08-12 08:21:42 +0000

    [Add]4-19:いいね機能の追加

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,9 +1,10 @@
 class PostImage < ApplicationRecord
   #ActiveStorage を使って画像を持たせる
   has_one_attached :image
-  
+
   belongs_to :user#PostImageモデルに対して、Userモデルとの関係性を追加
   has_many :post_comments, dependent: :destroy #PostImageモデルに、PostCommentモデルとの関連付け
+  has_many :favorites, dependent: :destroy
 
   def get_image #アクションとは少し違い、特定の処理を名前で呼び出すことができる4-11にて解説
     unless image.attached?
@@ -12,5 +13,10 @@ class PostImage < ApplicationRecord
     end
     image
   end
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id) #引数で渡されたユーザidがFavoritesテーブル内に存在（exists?）するかどうかを調べる
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "homes/about" => "homes#about", as: "about"
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do#自動作成された3つを1行に変更後、create.destroy追加
     resources :post_comments, only: [:create, :destroy] #投稿画像に対してコメントされるため、post_commentsは、post_imagesに結びつけて親子関係にする
+    resource :favorites, only: [:create, :destroy] #resource単数形にすると、/:idがURLに含まれなくなる
   end
   resources :users, only: [:show, :edit, :update] #自動作成された2つを1行にした
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
